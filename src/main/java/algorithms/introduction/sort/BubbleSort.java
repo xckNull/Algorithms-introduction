953a65528105404170aa66eee5431ca2d53ae134
xiechengkai <xiechengkai@qiyi.com>
2018-03-25 15:34:19 +0800

    bubble sort

--- src/main/java/algorithms/introduction/sort/BubbleSort.java
+++ src/main/java/algorithms/introduction/sort/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 
 	public static int[] bubbleSort(int[] arr) {
 		boolean flag = true;// 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已然完成。
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - 1 - i; j++) { //后面i个数据已经排序好了
 				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
--- src/test/java/algorithms/introduction/sort/test/BubbleSortTest.java
+++ src/test/java/algorithms/introduction/sort/test/BubbleSortTest.java
@@ -8,7 +8,7 @@ public class BubbleSortTest {
 
 	@Test
 	public void testBubbleSort() {
-		int[] arr = { 6, 3, 8, 2, 9, 1 };
+		int[] arr = { 6, 1, 2, 3, 9, 8 };
 		int[] sortArr = BubbleSort.bubbleSort(arr);
 		for (int i = 0; i < sortArr.length; i++) {
 			System.out.print(sortArr[i]);
