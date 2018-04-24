5c87e180dfa31a94a71a148566226ab928818aad
xiechengkai <xiechengkai@qiyi.com>
2018-04-24 09:47:23 +0800

    leetcode

---
+++ src/test/java/algorithms/introduction/leetcode/test/LongestPalindromeTest.java
@@ -0,0 +1,11 @@
+package algorithms.introduction.leetcode.test;
+
+import algorithms.introduction.leetcode.LongestPalindrome;
+
+public class LongestPalindromeTest {
+
+	public static void testLongestPalindrome() {
+		String s = "abcdad";
+		System.out.println(LongestPalindrome.longestPalindrome4(s));
+	}
+}
